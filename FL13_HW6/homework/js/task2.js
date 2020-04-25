e4df459ba213118f5816ce55e239ac1116c8110b
Olha Hrymchak <62155886+Olha98@users.noreply.github.com>
2020-04-25 20:13:25 +0300

    Add files via upload

---
+++ FL13_HW6/homework/js/task2.js
@@ -0,0 +1,17 @@
+/** @format */
+`use strict`;
+let inputWord = prompt(`Input a word`);
+let two = 2;
+let half = inputWord / two;
+
+let str = inputWord.length;
+
+if (inputWord === null) {
+  alert(`Invalid value`);
+} else if (inputWord.length % two === 0) {
+  let y = inputWord[str / two - 1] + inputWord[str / two];
+  alert(y);
+} else {
+  let x = inputWord[(str - 1) / two];
+  alert(x);
+}
